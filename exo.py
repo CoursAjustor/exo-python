6536af85ba6d3da78509ea6259fe12d51e65e5b1
Nolankbi <nolankbi31@gmail.com>
2021-05-11 09:28:55 +0200

    premier commit

---
+++ exo.py
@@ -0,0 +1,101 @@
+import random
+
+#Exercice 1
+
+#écrire une fonction qui pour un paramètre passé dis si oui ou non l'utilisateur peut accéder au site
+#exemple:
+#age < 21 => non
+#sinon => oui
+
+#Exercice 2
+
+#écrire une fonction qui pour un paramètre passé dis si oui ou non l'utilisateur peut accéder au site suivant le pay
+#exemple:
+#age < 18 => non
+#age < 21 => oui sauf EU
+#sinon => oui
+
+#Exercice 3
+
+#écrire une fonction qui compte de 1 au paramètre passé
+#exemple:
+#de 1 à 15
+#sortie:
+#1
+#2
+#3
+#4#
+#...
+#15
+
+for i in range(0, 16):
+      print(i)
+
+#Exercice 4
+
+#écrire une fonction qui compte de 1 au paramètre passé et avec seulement les nombre pairs
+#exemple:
+#de 1 à 15
+#sortie:
+#2
+#4
+#...
+#14
+
+#Exercice 4.1
+
+#écrire une fonction qui compte de 1 au paramètre passé et avec seulement les nombre impairs
+#exemple:
+#de 1 à 15
+#sortie:
+#1#
+#3
+#...
+#15
+
+#Exercice 5
+
+#écrire un code qui affiche une pyramide d'étoile de hauteur le paramètre passé
+#ex:
+#entrée:
+#3
+#sortie:
+
+#  *
+# ***
+#*****
+#entrée:
+#4
+#sortie:
+
+#   *
+#  ***
+# *****#
+#*******
+
+#Exercice final (bonus)
+
+#écrire un code qui affiche un losange d'étoile de hauteur le paramètre passé (attention il y a une subtilité)
+#ex:
+#entrée:
+#3
+#sortie:
+
+#  *
+# ***
+#  *
+#entrée:
+#4
+#sortie:
+#size need to be odd rerun using the default value
+#  *
+# ***
+#  *
+#entrée:
+#5
+#sortie:
+#  *
+# ***
+#*****
+# ***
+#  *
